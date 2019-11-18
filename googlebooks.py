5a671c0e16866f232e7d8142fc2a14c87d2b7f99
Davichet-e <48657914+Davichet-e@users.noreply.github.com>
2019-11-18 20:52:18 +0100

    Improved type checking coverage

--- googlebooks.py
+++ googlebooks.py
@@ -3,17 +3,18 @@ Google Books Api
 
 See: https://developers.google.com/books/
 """
-
-import json
-from typing import Dict, Optional
+from typing import Any, Dict, List, Optional, Union
 
 import requests
 
 
-_BASEURL: str = "https://www.googleapis.com/books/v1"
+_BASEURL = "https://www.googleapis.com/books/v1"
+
+VolumeResponse = Dict[str, Any]
+ListResponse = Dict[str, Union[str, List[VolumeResponse], int]]
 
 
-def _get(path: str, params: Optional[dict] = None) -> dict:
+def _get(path: str, params: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
     if params is None:
         params = {}
 
@@ -21,34 +22,37 @@ def _get(path: str, params: Optional[dict] = None) -> dict:
 
     resp.raise_for_status()
 
-    return json.loads(resp.content.decode("utf-8"))
+    return resp.json(encoding="utf-8")
 
 
-def get(volume_ID: str, **optional_parameters: str) -> dict:
+def get(volume_id: str, **kwargs: str) -> VolumeResponse:
     """
     Retrieve a Volume resource based on ID
     volumeId -- ID of volume to retrieve.
-    Optional Parameters:
-    partner --  Brand results for partner ID.
 
-    projection -- Restrict information returned to a set of selected fields. 
+    Optional Parameters:
+    partner: `str` --  Brand results for partner ID.
+
+    projection: `str` -- Restrict information returned to a set of selected fields. 
                 Acceptable values are:
                 "full" - Includes all volume data.
                 "lite" - Includes a subset of fields in volumeInfo and accessInfo.
 
-    source --   String to identify the originator of this request.
+    source: `str` -- String to identify the originator of this request.
+
     See: https://developers.google.com/books/docs/v1/reference/volumes/get
     """
-    path: str = "/volumes/" + volume_ID
+    path = f"/volumes/{volume_id}"
 
-    if optional_parameters:
-        for optional_parameter in optional_parameters:
-            if optional_parameter not in "partner projection source".split():
+    if kwargs:
+        for optional_parameter in kwargs:
+            if optional_parameter not in "partner projection source":
                 raise ValueError(f"Parameter '{optional_parameter}' not valid")
 
-    return _get(path, optional_parameters)
+    return _get(path, kwargs)
 
-def list_books(query: str, **optional_parameters: str) -> dict:
+
+def list_books(query: str, **kwargs: Union[str, int, bool]) -> ListResponse:
     """
     Perform a book search.
     query -- Full-text search query string.
@@ -58,73 +62,87 @@ def list_books(query: str, **optional_parameters: str) -> dict:
 
         intitle: Returns results where the text following this keyword is
                 found in the title.
+
         inauthor: Returns results where the text following this keyword is
                 found in the author.
+
         inpublisher: Returns results where the text following this keyword
                 is found in the publisher.
+
         subject: Returns results where the text following this keyword is
                 listed in the category list of the volume.
+
         isbn:   Returns results where the text following this keyword is the
                 ISBN number.
+
         lccn:   Returns results where the text following this keyword is the
                 Library of Congress Control Number.
+
         oclc:   Returns results where the text following this keyword is the
                 Online Computer Library Center number.
 
     Optional Parameters:
 
-    download -- Restrict to volumes by download availability. 
+    download: `str` -- Restrict to volumes by download availability. 
                 Acceptable values are:
                 "epub" - All volumes with epub.
-    filter --   Filter search results. 
+
+    filter: `str` -- Filter search results. 
                 Acceptable values are:
                 "ebooks" - All Google eBooks.
                 "free-ebooks" - Google eBook with full volume text viewability.
                 "full" - Public can view entire volume text.
                 "paid-ebooks" - Google eBook with a price.
                 "partial" - Public able to see parts of text.
-    langRestrict -- Restrict results to books with this language code.
-    libraryRestrict	-- Restrict search to this user's library. 
+
+    langRestrict: `str` -- Restrict results to books with this language code.
+
+    libraryRestrict: `str` -- Restrict search to this user's library. 
                 Acceptable values are:
                 "my-library" - Restrict to the user's library, any shelf.
                 "no-restrict" - Do not restrict based on user's library.
-    maxResults -- Maximum number of results to return. Acceptable values are 0 to 40, inclusive.
-    orderBy	 -- Sort search results. 
+
+    maxResults: `int` -- Maximum number of results to return. Acceptable values are 0 to 40, inclusive.
+
+    orderBy: `str` -- Sort search results.
                 Acceptable values are:
                 "newest" - Most recently published.
                 "relevance" - Relevance to search terms.
-    partner	--  Restrict and brand results for partner ID.
-    printType -- Restrict to books or magazines. 
+
+    partner: `str`	--  Restrict and brand results for partner ID.
+
+    printType: `str` -- Restrict to books or magazines. 
                 Acceptable values are:
                 "all" - All volume content types.
                 "books" - Just books.
                 "magazines" - Just magazines.
-    projection -- Restrict information returned to a set of selected fields. 
+
+    projection: `str` -- Restrict information returned to a set of selected fields. 
                 Acceptable values are:
                 "full" - Includes all volume data.
                 "lite" - Includes a subset of fields in volumeInfo and accessInfo.
 
-    showPreorders -- Set to true to show books available for preorder. Defaults to false.
-    source --  String to identify the originator of this request.
-    startIndex -- Index of the first result to return (starts at 0).
+    showPreorders: `bool` -- Set to true to show books available for preorder. Defaults to false.
+
+    source: `str` --  String to identify the originator of this request.
+
+    startIndex: `int` -- Index of the first result to return (starts at 0).
 
     See: https://developers.google.com/books/docs/v1/reference/volumes/list
     """
-    path: str = "/volumes"
+    path = "/volumes"
 
-    params: Dict[str, str] = {"q": query}
+    params: Dict[str, Union[str, int, bool]] = {"q": query}
 
-    if optional_parameters:
-        for optional_parameter in optional_parameters:
-            if (
-                optional_parameter
-                not in (
-                    "download filter langRestrict libraryRestrict maxResults "
-                    "orderBy partner printType projection showPreorders source startIndex"
-                ).split()
+    if kwargs:
+        for optional_parameter in kwargs:
+            if optional_parameter not in (
+                "download filter langRestrict libraryRestrict maxResults "
+                "orderBy partner printType projection showPreorders source startIndex"
             ):
                 raise ValueError(f"Parameter '{optional_parameter}' not valid")
 
-        params.update(optional_parameters)
+        params.update(kwargs)
 
     return _get(path, params)
+
